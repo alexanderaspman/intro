b07c0000c9ee46f240177dce25faf7383339352f
Alexander Aspman <aspmanalexander@gmail.com>
2021-09-24 10:44:24 +0200

    Update main.js
    lagt till lite kommentarer

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function checkForVisibility() {
     var headers = document.querySelectorAll(".delay");// deklarerar en variabel som sedan användas i functionen för att se om den är true
     var headers1 = document.querySelectorAll(".delaymore");
-    headers.forEach(function(header) {
+    headers.forEach(function(header) {  //ger header värdet i functionen som ska skickas tillbaka lopar functionen och tittar på ifall false ändras till true
 
         
-      if (isElementInViewport(header)) {// om header blir true byts cssen i .delay elementet ut till delay--enter
-       return header.classList.add("delay--enter");
+      if (isElementInViewport(header)) {// anropar isElementInViewport om header blir true byts cssen i .delay elementet ut till delay--enter    
+       return header.classList.add("delay--enter"); // skickar tillbaka header värdet till variablen headers och ger den ett nytt värde
         
     }
        
     });
-    headers1.forEach(function(header) {
+    headers1.forEach(function(header) { //
 
         
       if (isElementInViewport(header)) {
@@ -23,20 +23,20 @@ function checkForVisibility() {
 
   
   
-  function isElementInViewport(el) {               // känner efter ifall användarens skärm täcker bilden för att sedan användas i functionen ovanför trigga utbytet av css
+  function isElementInViewport(el) {               // känner efter ifall användarens skärm täcker bilden över elementet för att sedan användas i functionen ovanför trigga utbytet av css
     var rect = el.getBoundingClientRect();
   
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
+      rect.top >= 0 && //tiitar att det är noll pixlar om elementet är centrerat och var toppen borde vara  på skärmen 
+      rect.left >= 0 &&  //-ll-
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
     );
   }
   
-  if (window.addEventListener) {
-    addEventListener('DOMContentLoaded', checkForVisibility, false); 
-    addEventListener('load', checkForVisibility, false);
-    addEventListener('scroll', checkForVisibility, false);
+  if (window.addEventListener) { // är en callback function
+    addEventListener('DOMContentLoaded', checkForVisibility, false);  //säger att vid första load är det just nu  false men ifall true triggas foreach functionen
+    addEventListener('load', checkForVisibility, false); //vid refresh ska scriptet titta ifall detta är sant
+    addEventListener('scroll', checkForVisibility, false); //vid varje scroll triggas ett måste att titta ifall det är false
   }
   
